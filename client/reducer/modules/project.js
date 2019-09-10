78b4e59da59a7a0e5d048b8cebe728329ed87a26
李子屏 <877286986@qq.com>
2019-09-10 09:43:05 +0800

    fix: 重新打包发布

--- client/reducer/modules/project.js
+++ client/reducer/modules/project.js
@@ -24,7 +24,7 @@ const CHANGE_MEMBER_EMAIL_NOTICE = 'yapi/project/CHANGE_MEMBER_EMAIL_NOTICE';
 const GET_SWAGGER_URL_DATA = 'yapi/project/GET_SWAGGER_URL_DATA'
 // Reducer
 const initialState = {
-  defaultProjectId: 10,
+  defaultProjectId: 11,
   isUpdateModalShow: false,
   handleUpdateIndex: -1,
   projectList: [],
